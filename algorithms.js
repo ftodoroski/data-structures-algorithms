741f49cf7a1bd0e02a7b42307c977a2594f138c5
Filip Todoroski <ftodoroski@ymail.com>
2020-07-15 17:37:14 -0400

    added 2nd maxSubarrSum algorithm

--- algorithms.js
+++ algorithms.js
@@ -302,3 +302,43 @@ console.log(maxSubarraySum([2, 3], 3))                              // null
 // For sliding window the array needs to be transeversed through the end
 
 // I see that often Math.max or Math.min is being used for the sliding window
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Sliding Window
+// maxSubarraySum
+// 8. Write a function called maxSubarraySum which accepts an array
+//    of integers and a number called n. The function should calculate 
+//    the maximum sum of n consecutive elements in the array
+
+
+function maxSubarraySum(arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length === 0) return null;
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+
+
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)) // 10
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4)) // 17
+console.log(maxSubarraySum([4, 2, 1, 6], 1)) // 6
+console.log(maxSubarraySum([4, 2, 1, 6, 2], 4)) // 13
+console.log(maxSubarraySum([], null)) // null
+
+// Time Complexity
+//      - O(n)
+
+// Tip
+//      - You keep sliding like decreasing in the back and increasing at the front the subarray
