d34b8fb35bd0b9737db96e21f71430f4cb902146
Filip Todoroski <ftodoroski@ymail.com>
2020-07-21 12:38:40 -0400

    nestedEvenSum

--- algorithms.js
+++ algorithms.js
@@ -394,3 +394,50 @@ function iterate(months) {
 
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 iterate(months)
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Recursion 
+// nestedEvenSum
+// 11. Write a recursive function called nestedEvenSum. Return the sum of all even 
+//     numbers in an object which may contain nested objects  
+
+
+function nestedEvenSum(obj) {
+    let answer = 0
+    if (Object.keys(obj).length === 0) return answer
+    for (const key in obj) {
+        if (typeof obj[key] === 'number' && obj[key] % 2 === 0) {
+            answer += obj[key]
+        } else if (typeof obj[key] === 'object') {
+            answer += nestedEvenSum(obj[key])
+        }
+    }
+    return answer
+}
+
+var obj1 = {
+    outer: 2,
+    obj: {
+        inner: 2,
+        otherObj: {
+            superInner: 2,
+            notANumber: true,
+            alsoNotANumber: "yup"
+        }
+    }
+}
+
+var obj2 = {
+    a: 2,
+    b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+    c: { c: { c: 2 }, cc: 'ball', ccc: 5 },
+    d: 1,
+    e: { e: { e: 2 }, ee: 'car' }
+};
+
+console.log(nestedEvenSum(obj1)); // 6
+console.log(nestedEvenSum(obj2)); // 10
