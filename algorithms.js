02c7d28fbfafcb7a6cb72694ed0418c566c373ff
Filip Todoroski <ftodoroski@ymail.com>
2020-08-14 14:30:55 -0400

    Added twoSum

--- algorithms.js
+++ algorithms.js
@@ -1278,3 +1278,63 @@ console.log(isBalanced("{()[{({})[]()}]}([])"))
 console.log(isBalanced("{{}(}"))
 
 // O(n) Time | O(n) Space
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Two Number Sum
+
+// Write a function that takes in a non - empty array of distinct integers and an
+// integer representing a target sum. If any two numbers in the input array sum
+// up to the target sum, the function should return them in an array, in any
+// order.If no two numbers sum up to the target sum, the function should return
+// an empty array.
+
+
+// Note that the target sum has to be obtained by summing two different integers
+// in the array; you can't add a single integer to itself in order to obtain the
+// target sum.
+
+
+// You can assume that there will be at most one pair of numbers summing up to
+// the target sum.
+
+// Example
+// Input
+    // array = [3, 5, -4, 8, 11, 1, -1, 6]
+    // target 10
+
+// Output 
+    // [11, -1]
+
+function twoNumberSum(array, targetSum) {
+    const nums = {}
+
+    for (const num of array) {
+        const potentialMatch = targetSum - num
+
+        if (potentialMatch in nums) {
+            return [potentialMatch, num]
+        } else {
+            nums[num] = true
+        }
+    }
+
+    return []
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
