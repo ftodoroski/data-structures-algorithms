49881f53a486e4db059123d8f35aca5b9ae97970
Filip Todoroski <ftodoroski@ymail.com>
2020-08-15 08:25:37 -0400

    Added NthFib method

--- algorithms.js
+++ algorithms.js
@@ -1329,6 +1329,34 @@ function twoNumberSum(array, targetSum) {
 
 
 
+// ******************************************************************************************************************************************************************
+// Nth Fibonacci
+
+// The Fibonacci sequence is defined as follows: the first number of the sequence
+// is 0, the second number is 1, and the nth number is the sum of the (n - 1)th
+// and(n - 2)th numbers. Write a function that takes in an integer n  and 
+// returns the nth Fibonacci number.
+
+
+// Important note: the Fibonacci sequence is often defined with its first two numbers 
+// as F0 = 0 and F1 = 1. For the purpose of this question, the first Fibonacci number is 
+// F0; therefore, getNthFib(1) is equal to F0, getNthFib(2) is equal to F1
+
+// Sample  Input 
+// getNthFib(6)
+
+// Sample Output
+// 5
+
+function getNthFib(n, memo = { "1": 0, "2": 1 }) {
+    if (n in memo) return memo[n]
+    else memo[n] = getNthFib(n - 1) + getNthFib(n - 2)
+
+    return memo[n]
+}
+
+// Time:    O(n)
+// Space:   O(n) 
 
 
 
