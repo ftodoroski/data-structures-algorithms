c04ba7c3606650cf2dccf55b4b18fc8e3906d966
Filip Todoroski <ftodoroski@ymail.com>
2020-08-19 21:40:49 -0400

    Added reverseInt

--- algorithms.js
+++ algorithms.js
@@ -1480,3 +1480,27 @@ function chunk(array, size) {
 
     return result
 }
+
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Reverse an Integer
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+    if (n === 0) return 0
+
+    let stringified = n.toString().split("").reverse().join("")
+    return parseInt(stringified) * Math.sign(n)
+}
