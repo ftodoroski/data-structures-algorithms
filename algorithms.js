b4e7ac163b352e5f46f630b9fc5db092239fc9cc
Filip Todoroski <ftodoroski@ymail.com>
2020-08-20 17:20:24 -0400

    Added reverse

--- algorithms.js
+++ algorithms.js
@@ -1504,3 +1504,28 @@ function reverseInt(n) {
     let stringified = n.toString().split("").reverse().join("")
     return parseInt(stringified) * Math.sign(n)
 }
+
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Reverse a String
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    let reversedStr = ""
+    for (let i = str.length - 1; i >= 0; i--) {
+        const char = str[i]
+        reversedStr += char
+    }
+
+    return reversedStr
+}
