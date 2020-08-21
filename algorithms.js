853550bf6ea136a35b0ef12a0660c7eaa28689f2
Filip Todoroski <ftodoroski@ymail.com>
2020-08-21 13:33:43 -0400

    Added branchSum

--- algorithms.js
+++ algorithms.js
@@ -1529,3 +1529,48 @@ function reverse(str) {
 
     return reversedStr
 }
+
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Branch Sums 
+
+// Write a function that takes in a Binary Tree and returns a list of its branch
+// sums ordered from leftmost branch sum to rightmost branch sum.
+
+// A branch sum is the sum of all values in a Binary Tree branch.A Binary Tree
+// branch is a path of nodes in a tree that starts at the root node and ends at
+// any leaf node.
+
+// Each BinaryTree node has an integer value, a left child node and a right child node.
+// Children node can either be BinaryTree nodes themselves or Node / Null
+
+class BinaryTree {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const branchSums = root => {
+    const sums = []
+    calBranchSums(root, 0, sums)
+    return sums
+}
+
+const calBranchSums = (node, sum, sums) => {
+    if (!node) return
+
+    const runningSum = sum + node.value
+    if (!node.left && !node.right) {
+        sums.push(runningSum)
+        return
+    }
+
+    calBranchSums(node.left, runningSum, sums)
+    calBranchSums(node.right, runningSum, sums)
+}
