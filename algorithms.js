14abee300b65fc078b0ea9bb62458708cc55cb21
Filip Todoroski <ftodoroski@ymail.com>
2020-09-06 11:37:31 -0400

    Added the DFS method to DFS PostOrder

--- algorithms.js
+++ algorithms.js
@@ -1929,6 +1929,19 @@ class BinarySearchTree {
 
         }
     }
+
+    DFSPostOrder() {
+        let data = []
+
+        function traverse(node) {
+            if (node.left) traverse(node.left)
+            if (node.right) traverse(node.right)
+            data.push(node.value)
+        }
+
+        traverse(this.root)
+        return data
+    }
 }
 
 
