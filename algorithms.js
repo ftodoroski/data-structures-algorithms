a881dd05475b7185c258628201e1120bee0fa444
Filip Todoroski <ftodoroski@ymail.com>
2020-09-12 13:49:12 -0400

    Added spiralTeversal

--- algorithms.js
+++ algorithms.js
@@ -2128,3 +2128,61 @@ const reorderLogFiles = (logs) => {
 
 // Test Case 
 reorderLogFiles(["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"])
+
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Spiral Traverse 
+// Write a function that takes in an two-dimensional array(that can be square-shaped when n === m) 
+// and returns a one-dimensional array of all the array's elements spiral order
+
+// Spiral order starts at the top left corner of the two-dimensional array, goes to the right 
+// and proceeds in a spiral pattern all the way until every element has been visited
+
+// array = [
+//            [1, 2, 3, 4], 
+//            [12, 13, 14, 5], 
+//            [11, 16, 15, 6],
+//            [10, 9, 8, 7]
+//        ]
+
+function spiralTraverse(array) {
+    const result = []
+    let startRow = 0
+    let endRow = array.length - 1
+    let startCol = 0
+    let endCol = array[0].length - 1
+
+    while (startRow <= endRow && startCol <= endCol) {
+        for (let col = startCol; col <= endCol; col++) {
+            result.push(array[startRow][col])
+        }
+
+        for (let row = startRow + 1; row <= endRow; row++) {
+            result.push(array[row][endCol])
+        }
+
+        for (let col = endCol - 1; col >= startCol; col--) {
+            if (startRow === endRow) break
+            result.push(array[endRow][col])
+        }
+
+        for (let row = endRow - 1; row > startRow; row--) {
+            if (startCol === endCol) break
+            result.push(array[row][startCol])
+        }
+
+        startRow++
+        endRow--
+        startCol++
+        endCol--
+    }
+
+    return result
+}
+
+spiralTraverse([[1, 2, 3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7]])
+
