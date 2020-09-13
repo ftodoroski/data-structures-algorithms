c9988a0837f562483a550eeaea1de23d95b14f26
Filip Todoroski <ftodoroski@ymail.com>
2020-09-13 16:28:52 -0400

    Added longestPeak

--- algorithms.js
+++ algorithms.js
@@ -2186,3 +2186,53 @@ function spiralTraverse(array) {
 
 spiralTraverse([[1, 2, 3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7]])
 
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Longest Peak 
+// Write a function that takes in an array of integers and returns the length of the longest peak in the array
+
+// A peak is defined as adjacent integers in the array are are stictly inreasing until they reach a 
+// tip(the highest value in the peak) at which point they become strictly decreasing. 
+// At least three integers are required to form a peak
+
+// For example the integers [1, 4, 10, 2] form a peak, but the integers [4, 0, 10] don't 
+// and neither do the integers [1, 2, 2, 0].
+// Similarly the intgers [1, 2, 3] don't form a peak because there aren't any strictly decreasing integers after the 3. 
+
+function longestPeak(array) {
+    let longestPeakLength = 0
+    let i = 1
+    while (i < array.length - 1) {
+        const isPeak = array[i - 1] < array[i] && array[i] > array[i + 1]
+
+        if (!isPeak) {
+            i++
+            continue
+        }
+
+        let leftIdx = i - 2
+        while (leftIdx >= 0 && array[leftIdx] < array[leftIdx + 1]) {
+            leftIdx--
+        }
+
+        let rightIdx = i + 2
+        while (rightIdx < array.length && array[rightIdx] < array[rightIdx - 1]) {
+            rightIdx++
+        }
+
+        const currentPeakLength = rightIdx - leftIdx - 1
+        longestPeakLength = Math.max(longestPeakLength, currentPeakLength)
+        i = rightIdx
+    }
+
+    return longestPeakLength
+}
+
+
+console.log(longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]))
+
+
