5df86bbbb8db16f9f726c8e19ee7c748cc103108
Filip Todoroski <ftodoroski@ymail.com>
2020-09-17 20:01:44 -0400

    added Node Depth

--- algorithms.js
+++ algorithms.js
@@ -2336,3 +2336,32 @@ function isPalindrome(string) {
 
     return string === reversedWord
 }
+
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Node Depths
+//  The distance between a node in a Binary Tree and the tree's root is called the
+//  node's depth.
+
+// Write a function that takes in a Binary Tree and returns the sum of its nodes'
+// depths.
+
+// Each BinaryTree node has integer value, a left child node and a right child node. 
+// Children nodes can either be BinaryTree nodes themselves or None / Null
+
+function nodeDepths(root, depth = 0) {
+    if (!root) return 0
+    return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1)
+}
+
+class BinaryTree {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
