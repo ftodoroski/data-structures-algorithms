0817f9a9653da31dd43fa586cb1f9a30c5840c85
Filip Todoroski <ftodoroski@ymail.com>
2020-09-20 20:12:19 -0400

    added defangIPaddr

--- algorithms.js
+++ algorithms.js
@@ -2396,3 +2396,35 @@ function isValidSubsequence(array, sequence) {
 
     return seqIdx === sequence.length
 }
+
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Defanging an IP Address
+// Given a valid(IPv4) IP address, return a defanged version of that IP address.
+// A defanged IP address replaces every period "." with "[.]".
+
+// Example 1
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+
+// Example 2:
+// Input: address = "255.100.50.0"
+// Output: "255[.]100[.]50[.]0"
+
+var defangIPaddr = function (address) {
+    let newIP = ''
+
+    for (let i = 0; i < address.length; i++) {
+        if (address[i] === '.') {
+            newIP += '[.]'
+        } else {
+            newIP += address[i]
+        }
+    }
+
+    return newIP
+};
