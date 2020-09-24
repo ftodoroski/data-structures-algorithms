e458603013a6682d244a2a72e4f4eb05fa817d98
Filip Todoroski <ftodoroski@ymail.com>
2020-09-24 15:56:42 -0400

    Added DFS - InOrder

--- algorithms.js
+++ algorithms.js
@@ -2606,3 +2606,41 @@ class BinaryTree {
 
 
 
+
+
+
+// ******************************************************************************************************************************************************************
+// Depth First Search - InOrder
+// 27. Implement a depth first search - InOrder
+
+class Node {
+    constructor(value) {
+        this.value = value
+        this.right = null
+        this.left = null
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+}
+
+
+let bst = new BinarySearchTree()
+bst.insert(25)
+bst.insert(32)
+bst.insert(17)
+bst.insert(39)
+bst.insert(30)
+bst.insert(23)
+bst.insert(14)
+
+// ||
+// bst.insert(10);
+// bst.insert(6);
+// bst.insert(15);
+// bst.insert(3);
+// bst.insert(8);
+// bst.insert(20);
