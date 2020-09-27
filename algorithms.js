da91b6f3e477dcf802b1bce1ea2ef494a5b9670b
Filip Todoroski <ftodoroski@ymail.com>
2020-09-27 15:59:34 -0400

    Added dfsInOrder method to DFS - InOrder

--- algorithms.js
+++ algorithms.js
@@ -2670,6 +2670,19 @@ class BinarySearchTree {
 
         }
     }
+
+    DFSInOrder() {
+        let data = []
+
+        function traverse(node) {
+            if (node.left) traverse(node.left)
+            data.push(node.value)
+            if (node.right) traverse(node.right)
+        }
+
+        traverse(this.root)
+        return data
+    }
 }
 
 
