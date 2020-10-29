8b8e3b34d65a5c5804a1ced0bc5fd0f6aa978c18
Filip Todoroski <ftodoroski@ymail.com>
2020-10-29 16:27:34 -0400

    Added addEdge method to Undirected Weighted Graph

--- algorithms.js
+++ algorithms.js
@@ -3326,6 +3326,11 @@ class WeightedGraph {
     addVertex(vertex) {
         if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
+
+    addEdge(vertex1, vertex2, weight) {
+        this.adjacencyList[vertex1].push({ node: vertex2, weight });
+        this.adjacencyList[vertex2].push({ node: vertex1, weight });
+    }
 }
 
 
