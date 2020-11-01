d7eefc33efacb5a4609c1ec0c192b9c38e3475e7
Filip Todoroski <ftodoroski@ymail.com>
2020-11-01 15:32:45 -0500

    Added bubbleUp to PriorityQueue for Undirected Weighted Graph

--- algorithms.js
+++ algorithms.js
@@ -3387,6 +3387,19 @@ class PriorityQueue {
         this.values.push(newNode);
         this.bubbleUp();
     }
+
+    bubbleUp() {
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2);
+            let parent = this.values[parentIdx];
+            if (element.priority >= parent.priority) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
 }
 
 
