889c84b929732940e31b8cd247635977cb183dd1
Filip Todoroski <ftodoroski@ymail.com>
2020-11-02 15:33:28 -0500

    Added dequeue to PriorityQueue for Undirected Weighted Graph

--- algorithms.js
+++ algorithms.js
@@ -3400,6 +3400,16 @@ class PriorityQueue {
             idx = parentIdx;
         }
     }
+
+    dequeue() {
+        const min = this.values[0];
+        const end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return min;
+    }
 }
 
 
