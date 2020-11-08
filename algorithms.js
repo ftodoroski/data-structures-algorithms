38cc983f975f06eacf7ffdc261a5438ddf306421
Filip Todoroski <ftodoroski@ymail.com>
2020-11-08 13:56:46 -0500

    Added Reverse an Integer

--- algorithms.js
+++ algorithms.js
@@ -3568,3 +3568,27 @@ function palindrome(str) {
 
     return str === reversedStr
 }
+
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Reverse an Integer
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+    if (n === 0) return 0
+
+    let stringified = n.toString().split("").reverse().join("")
+    return parseInt(stringified) * Math.sign(n)
+}
