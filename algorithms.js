13588165582da4ab478a322a2c0ba38ccaec599e
Filip Todoroski <ftodoroski@ymail.com>
2020-11-11 14:07:09 -0500

    Added levenshteinDistance

--- algorithms.js
+++ algorithms.js
@@ -3641,3 +3641,45 @@ console.log(insertionSort(array))
 
 // Time Compexity:      O(n)
 // Space Compelxity:    O(1)
+
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Levenshtein Distance - Dynamic Programming Problem 
+
+// Write a function that takes in two strings and returns the minimum number of edit operations 
+// that need to be perfomed on the first sting to obtain the second string 
+
+// There are three edit operations: insertion of a char, deletion of char, and substitution of a char for another
+
+// Sample Input 
+// str1 = "abc"
+// str2 = "yabd"
+
+// Sample Output 
+// 2 
+
+function levenshteinDistance(str1, str2) {
+    const edits = []
+    for (let i = 0; i < str2.length + 1; i++) {
+        const row = []
+        for (let j = 0; j < str1.length + 1; j++) {
+            row.push(j)
+        }
+        row[0] = i
+        edits.push(row)
+    }
+    for (let i = 1; i < str2.length + 1; i++) {
+        for (let j = 1; j < str1.length + 1; j++) {
+            if (str2[i - 1] === str1[j - 1]) {
+                edits[i][j] = edits[i - 1][j - 1]
+            } else {
+                edits[i][j] = 1 + Math.min(edits[i - 1][j - 1], edits[i - 1][j], edits[i][j - 1])
+            }
+        }
+    }
+    return edits[str2.length][str1.length]
+}
