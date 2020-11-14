6c9c67e18c31fc1e96b98919da28232927dda286
Filip Todoroski <ftodoroski@ymail.com>
2020-11-14 14:42:34 -0500

    Added minHeightBST

--- algorithms.js
+++ algorithms.js
@@ -3761,3 +3761,38 @@ function productSum(array, depth = 1) {
 
     return sum * depth
 }
+
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// minHeightBst
+
+// Write a function that takes in a non - empty sorted array of distinct integers,
+// constructs a BST from the integers, and returns the root of the BST.
+
+// The function should minimize the height of the BST.
+
+// You've been provided with a BST  class that you'll have to use to
+// construct the BST.
+
+// Each BST node has an integer value, a left child node and a right child node.
+// A node is said to be a valid BSTnode if and only if it satisfies the BST property.
+// its value is strictly greater than the values of every node to its left. its value is less
+// than or equal to the values of every node to its right. and its children nodes are either 
+// valid BST nodes themselves or None / null
+
+
+// A BST is valid if and only if all of its nodes are valid BST nodes
+
+// Note that the BST class already has an insert method which you can use if you want. 
+
+// Input sample
+// array = [1, 2, 5, 7, 10, 13, 14, 15, 22]
+
+function minHeightBst(array) {
+    return constructMinHeightBST(array, null, 0, array.length - 1)
+}
+
