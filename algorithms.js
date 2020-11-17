b78041007b5fa6bddba822113874dae37f5f6465
Filip Todoroski <ftodoroski@ymail.com>
2020-11-17 14:45:14 -0500

    Added insert  to BST in  minHeightBST

--- algorithms.js
+++ algorithms.js
@@ -3817,4 +3817,20 @@ class BST {
         this.left = null;
         this.right = null;
     }
+
+    insert(value) {
+        if (value < this.value) {
+            if (this.left === null) {
+                this.left = new BST(value);
+            } else {
+                this.left.insert(value);
+            }
+        } else {
+            if (this.right === null) {
+                this.right = new BST(value);
+            } else {
+                this.right.insert(value);
+            }
+        }
+    }
 }
