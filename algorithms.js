92e41efff197fa2bf3aa87bbfcfda906a59ccbcd
Filip Todoroski <ftodoroski@ymail.com>
2020-11-21 10:15:36 -0500

    Added maxSubsetSumNoAdjacent

--- algorithms.js
+++ algorithms.js
@@ -3871,3 +3871,34 @@ class BinaryTree {
         this.right = null;
     }
 }
+
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Max Subset Sum No Adjacent
+
+
+// Write a function that takes in an array of positive integers and returns the
+// maximum sum of non - adjacent elements in the array.
+
+// If the input array is empty, the function should return 0
+
+function maxSubsetSumNoAdjacent(array) {
+    if (!array.length) return 0
+    if (array.length === 1) return array[0]
+
+    const maxSums = array.slice()
+    maxSums[1] = Math.max(array[0], array[1])
+
+    for (let i = 2; i < array.length; i++) {
+        maxSums[i] = Math.max(maxSums[i - 1], maxSums[i - 2] + array[i])
+    }
+
+    return maxSums[maxSums.length - 1]
+}
+
+let array = [75, 105, 120, 75, 90, 135]
+maxSubsetSumNoAdjacent(array) // 30
