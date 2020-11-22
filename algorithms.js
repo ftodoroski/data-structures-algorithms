c1d973f92cb3e40fd339b7c206c1c6395ca4916d
Filip Todoroski <ftodoroski@ymail.com>
2020-11-22 10:18:16 -0500

    Added numberOfWaysToMakeChange

--- algorithms.js
+++ algorithms.js
@@ -3902,3 +3902,36 @@ function maxSubsetSumNoAdjacent(array) {
 
 let array = [75, 105, 120, 75, 90, 135]
 maxSubsetSumNoAdjacent(array) // 30
+
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Number of ways to make a change 
+
+
+
+// Given an array of distinct positive integers representing coin denominations and a
+// single non - negative integer n representing a target amount of
+// money, write a function that returns the number of ways to make change for
+// that target amount using the given coin denominations.
+
+// Note that an unlimited amount of coins is at your disposal.
+
+function numberOfWaysToMakeChange(n, denoms) {
+    const ways = new Array(n + 1).fill(0)
+    ways[0] = 1
+    for (let denom of denoms) {
+        for (let amount = 1; amount < n + 1; amount++) {
+            if (denom <= amount) {
+                ways[amount] += ways[amount - denom]
+            }
+        }
+    }
+
+    return ways[n]
+}
+
+numberOfWaysToMakeChange(6, [1, 5])
