69662f08475c40a187d73ac394af4275f8fc3765
Filip Todoroski <ftodoroski@ymail.com>
2020-11-23 10:22:48 -0500

    Added minNumberOfWaysToMakeChange

--- algorithms.js
+++ algorithms.js
@@ -3934,4 +3934,40 @@ function numberOfWaysToMakeChange(n, denoms) {
     return ways[n]
 }
 
-numberOfWaysToMakeChange(6, [1, 5])
+numberOfWaysToMakeChange(6, [1, 5]) // 2
+
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Min number of coins for change 
+
+// Given an array of positive integers representing coin denominations and a
+// single non - negative integer n  representing a target amount of
+// money, write a function that returns the smallest number of coins needed to
+// make change for (to sum up to) that target amount using the given coin
+// denominations.
+
+// Note that you have access to an unlimited amount of coins.In other words, if
+// the denominations are[1, 5, 10], , you have access to an unlimited
+// amount of 1s, 5s, 10s
+
+// If it's impossible to make change for the target amount, return -1
+
+function minNumberOfCoinsForChange(n, denoms) {
+    const numOfCoins = new Array(n + 1).fill(Infinity)
+    numOfCoins[0] = 0
+    for (const denom of denoms) {
+        for (let amount = 0; amount < numOfCoins.length; amount++) {
+            if (denom <= amount) {
+                numOfCoins[amount] = Math.min(numOfCoins[amount], numOfCoins[amount - denom] + 1)
+            }
+        }
+    }
+
+    return numOfCoins[n] !== Infinity ? numOfCoins[n] : -1;
+}
+
+minNumberOfCoinsForChange(7, [1, 5, 10]) // 3
