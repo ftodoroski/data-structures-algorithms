b3d56004f274ec703255f93e9d43385234fe3c8a
Filip Todoroski <ftodoroski@ymail.com>
2020-11-23 09:42:03 -0500

    Added hasSingleCycle

--- algorithms.js
+++ algorithms.js
@@ -3999,3 +3999,41 @@ function kadanesAlgorithm(array) {
 
 let array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4] 
 kadanesAlgorithm(array) // 19
+
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Single Cycle Check 
+
+// You're given an array of integers where each integer represents a jump of its
+// value in the array.For instance, the integer 2  represents a jump
+// of two indices forward in the array; the integer 3 represents a
+// jump of three indices backward in the array.
+
+// If a jump spills past the array's bounds, it wraps over to the other side. For
+// instance, a jump of - 1 at index 0 brings us to the last index in
+// the array.Similarly, a jump of 1 at the last index in the array brings us to
+// index 0
+
+// Write a function that returns a boolean representing whether the jumps in the
+// array form a single cycle.A single cycle occurs if, starting at any index in
+// the array and following the jumps, every element in the array is visited
+// exactly once before landing back on the starting index.
+
+function hasSingleCycle(array) {
+    let numElementsVisited = 0
+    let currentIdx = 0
+    while (numElementsVisited < array.length) {
+        if (numElementsVisited > 0 && currentIdx === 0) return false
+        numElementsVisited++
+        currentIdx = getNextIdx(currentIdx, array)
+    }
+
+    return currentIdx === 0
+}
+
+let array = [2, 3, 1, -4, -4, 2]
+hasSingleCycle(array) // true
