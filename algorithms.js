2dfc47a6932cf0c227c418df86f0a35ef8934ade
Filip Todoroski <ftodoroski@ymail.com>
2020-11-25 09:36:43 -0500

    Added kadanesAlgorithm

--- algorithms.js
+++ algorithms.js
@@ -3971,3 +3971,31 @@ function minNumberOfCoinsForChange(n, denoms) {
 }
 
 minNumberOfCoinsForChange(7, [1, 5, 10]) // 3
+
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Kadane's Algorithm 
+
+// Write a function that takes in a non - empty array of integers and returns the
+// maximum sum that can be obtained by summing up all of the integers in a
+// non - empty subarray of the input array.A subarray must only contain adjacent
+// numbers(numbers next to each other in the input array).
+
+function kadanesAlgorithm(array) {
+    let maxEndingHere = array[0]
+    let maxSoFar = array[0]
+    for (let i = 1; i < array.length; i++) {
+        const num = array[i]
+        maxEndingHere = Math.max(num, maxEndingHere + num)
+        maxSoFar = Math.max(maxSoFar, maxEndingHere)
+    }
+
+    return maxSoFar
+}
+
+let array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4] 
+kadanesAlgorithm(array) // 19
