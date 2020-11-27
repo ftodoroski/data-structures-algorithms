e032446f808c5bc472d64e335c6611c924c0e8a0
Filip Todoroski <ftodoroski@ymail.com>
2020-11-27 10:07:25 -0500

    Added traverseNode method to riverSizes

--- algorithms.js
+++ algorithms.js
@@ -4077,6 +4077,28 @@ function riverSizes(matrix) {
     return sizes
 }
 
+function traverseNode(i, j, matrix, visited, sizes) {
+    let currentRiverSize = 0
+    const nodesToExplore = [[i, j]]
+    while (nodesToExplore.length) {
+        const currentNode = nodesToExplore.pop()
+        i = currentNode[0]
+        j = currentNode[1]
+
+        if (visited[i][j]) continue
+        visited[i][j] = true
+
+        if (matrix[i][j] === 0) continue
+        currentRiverSize++
+
+        const unvisitedNeighbors = getUnvisitedNeighbors(i, j, matrix, visited)
+        for (const neighbor of unvisitedNeighbors) {
+            nodesToExplore.push(neighbor)
+        }
+    }
+
+    if (currentRiverSize > 0) sizes.push(currentRiverSize)
+}
 
 riverSizes(matrix) // [1, 2, 2, 2, 5]
 
