e66eceb1a67a482297849e53f5e44d6a62ed93f9
Filip Todoroski <ftodoroski@ymail.com>
2020-11-29 09:09:13 -0500

    Added AncestralTree class

--- algorithms.js
+++ algorithms.js
@@ -4118,3 +4118,32 @@ let matrix = [
         [1, 0, 1, 0, 1],
         [1, 0, 1, 1, 0]
     ]
+
+
+
+
+
+
+// ******************************************************************************************************************************************************************
+// Youngest Common Ancestor
+
+// You're given three inputs, all of which are instances of an AncestralTree class that have an ancestor property
+// pointing to their youngest ancestor.The first input is the top ancestor in an
+// ancestral tree(i.e., the only instance that has no ancestor--its
+// ancestor property points to None), and the other two inputs are descendants in the ancestral
+// tree.
+
+// Write a function that returns the youngest common ancestor to the two
+// descendants.
+
+// Note that a descendant is considered its own ancestor.So in the simple
+// ancestral tree below, the youngest common ancestor to nodes A and B is node A.
+
+class AncestralTree {
+    constructor(name) {
+        this.name = name;
+        this.ancestor = null;
+    }
+}
+
+
