16ca87fc52676e181e9663739e21f79f32ae9946
Filip Todoroski <ftodoroski@ymail.com>
2020-11-30 09:10:24 -0500

    Added getYoungestCommonAncestor

--- algorithms.js
+++ algorithms.js
@@ -4147,3 +4147,14 @@ class AncestralTree {
 }
 
 
+function getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo) {
+    const depthOne = getDescendantDepth(descendantOne, topAncestor)
+    const depthTwo = getDescendantDepth(descendantTwo, topAncestor)
+
+    if (depthOne > depthTwo) {
+        return backtrackAncestralTree(descendantOne, descendantTwo, depthOne - depthTwo)
+    } else {
+        return backtrackAncestralTree(descendantTwo, descendantOne, depthTwo - depthOne)
+    }
+}
+
